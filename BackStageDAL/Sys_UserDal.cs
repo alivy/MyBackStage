2d3aa54c3e12beb3d6f6ab3fbd9ce8526496920f
严浩淼 <527758393@qq.com>
2019-07-03 23:38:22 +0800

    修改阿毛的获取用户信息方法

--- BackStageDAL/Sys_UserDal.cs
+++ BackStageDAL/Sys_UserDal.cs
@@ -10,6 +10,7 @@ using DBModel.Result;
 using ViewModel;
 using System.Data;
 using Common;
+using System.Data.SqlClient;
 
 namespace BackStageDAL
 {
@@ -27,14 +28,39 @@ namespace BackStageDAL
             CurrentContext.Database.Log = (sql) => Log.Write(LogLevel.Info, sql);
             return result;
         }
-
+        /// <summary>
+        /// 获取用户信息
+        /// </summary>
+        /// <param name="userid"></param>
+        /// <param name="page"></param>
+        /// <returns></returns>
         public List<ResUserInfoAPI> GetUserInfo(string userid, ReqBasePage page)
         {
-            using (CurrentContext)
+
+            string sql = string.Format(@"exec Common_PageList '{0}','{1}','{2}',{3},{4},'{5}',{6}",
+                "Sys_User", "*", !string.IsNullOrWhiteSpace(userid) ? string.Format("  userid=''{0}''", userid) : "1=1",
+                page.pageIndex, page.pageSize, "userid", 0);
+            //第一 net fk4.8环境下已经改善字符拼接写法
+            string field = !string.IsNullOrWhiteSpace(userid) ? string.Format("  userid=''{0}''", userid) : "1 = 1";
+            string sql1 = $"exec Common_PageList '{"Sys_User"}''{"*"}''{ field}'{page.pageIndex},{page.pageSize}'{"userid"}',{0}";
+
+            //第二，可以用这种方式声明，简单明了
+            SqlParameter[] m_parms = new SqlParameter[7]
             {
-                string sql = string.Format("exec Common_PageList '{0}','{1}','{2}',{3},{4},'{5}',{6}", "Sys_User", "*", !string.IsNullOrWhiteSpace(userid) ? string.Format("  userid=''{0}''", userid) : "1=1", page.pageIndex, page.pageSize, "userid", 0);
-                CurrentContext.Database.Log = (sql1) => Log.Write(LogLevel.Info, sql);
-                var result = CurrentContext.Database.SqlQuery<ResUserInfoAPI>(sql).ToList();
+                new SqlParameter("@tab","Sys_User"),
+                new SqlParameter("@strFld","*"),
+                new SqlParameter("@strWhere",field),
+                new SqlParameter("@PageIndex",page.pageIndex),
+                new SqlParameter("@PageSize",page.pageSize),
+                new SqlParameter("@Sort","userid"),
+                new SqlParameter("@IsGetCount",0)
+            };
+            string sql2 = $"exec Common_PageList @tab,@strFld,@strWhere,@PageIndex,@PageSize,@Sort,@IsGetCount";
+
+            using (var db = CurrentContext)
+            {
+                var result = db.Database.SqlQuery<ResUserInfoAPI>(sql2, m_parms).ToList();
+                db.Database.Log = (x) => Log.Write(LogLevel.Info, x); ///这个是获取db的sql执行代码，内部委托
                 return result;
             }
         }
