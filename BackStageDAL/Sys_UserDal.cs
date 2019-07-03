bdb3336ff35efeaab3eb14edf4e3c3128a1c7649
Administrator <Administrator@USER-20171021WW>
2019-07-03 18:01:05 +0800

    修改分页sql

--- BackStageDAL/Sys_UserDal.cs
+++ BackStageDAL/Sys_UserDal.cs
@@ -32,16 +32,9 @@ namespace BackStageDAL
         {
             using (CurrentContext)
             {
-                string sql = string.Format(@"select * 
-                                    from (select top {0} * 
-                                    from (select top {1} * 
-                                    from Sys_User where 1=1 and {2}
-                                    order by UserId asc ) 
-                                    as temp_sum_student 
-                                    order by UserId desc ) temp_order
-                                    order by UserId asc", page.pageSize, page.pageIndex* page.pageSize, !string.IsNullOrWhiteSpace(userid)? string.Format("  userid='{0}'", userid):"");
+                string sql = string.Format("exec Common_PageList '{0}','{1}','{2}',{3},{4},'{5}',{6}", "Sys_User", "*", !string.IsNullOrWhiteSpace(userid) ? string.Format("  userid=''{0}''", userid) : "1=1", page.pageIndex, page.pageSize, "userid", 0);
                 CurrentContext.Database.Log = (sql1) => Log.Write(LogLevel.Info, sql);
-                var result = CurrentContext.Database.SqlQuery<ResUserInfoAPI>(sql).Skip(page.pageSize * (page.pageIndex - 1)).Take(page.pageSize).ToList();
+                var result = CurrentContext.Database.SqlQuery<ResUserInfoAPI>(sql).ToList();
                 return result;
             }
         }
